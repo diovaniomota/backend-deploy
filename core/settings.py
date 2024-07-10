6b9f9593cde1f3899cff486ee2c7fbaa564310a0
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-07-10 17:35:44 -0300

    Hotfix: CSRF Admin error

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # Allowed hosts for django project
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
                           "127.0.0.1,localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_ALLOWED_HOSTS",
+                                 "127.0.0.1,localhost").split(",")
 
 # List of origins that are authorized to make cross-site HTTP requests
 CORS_ALLOWED_ORIGINS = os.getenv(

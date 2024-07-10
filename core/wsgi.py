1de0e2b2c3035261a11de74feaed078f4d2097b9
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-07-10 16:56:19 -0300

    Hotfix: Port already in user

--- core/wsgi.py
+++ core/wsgi.py
@@ -19,4 +19,4 @@ django_app = StaticFilesHandler(get_wsgi_application())
 application = socketio.WSGIApp(sio, django_app)
 
 # Start server
-eventlet.wsgi.server(eventlet.listen(('', 8080)), application)
+eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
